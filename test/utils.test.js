7c19e10cd83853b9c18dcb62d74c3c5c154c3bc4
tislais <tistististis@gmail.com>
2021-03-24 23:42:50 -0700

    fix compareNumbers test

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,13 +3,13 @@ import { compareNumbers } from '../utils.js';
 
 const test = QUnit.test;
 
-test('should take in an integer, and return the guess as "1" when too high, "-1" when too low, "0" when correct', (expect) => {
+test('should take in an integer, and return "high", "low, or "correct"', (expect) => {
     const actualGuessHigh = compareNumbers(5, 10);
     const actualGuessLow = compareNumbers(10, 10);
     const actualGuessCorrect = compareNumbers(15, 10);
 
     // three arguments: actual, expected, some description
-    expect.equal(actualGuessHigh, -1, '5 is too low');
-    expect.equal(actualGuessLow, 0, '10 is correct');
-    expect.equal(actualGuessCorrect, 1, '15 is too high');
+    expect.equal(actualGuessHigh, 'low', '5 is too low');
+    expect.equal(actualGuessLow, 'correct', '10 is correct');
+    expect.equal(actualGuessCorrect, 'high', '15 is too high');
 });
