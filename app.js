44258cf661e41db09093a48d55d88b3fb727a316
tislais <tistististis@gmail.com>
2021-03-24 17:23:28 -0700

    add functionality to handle all edge cases

--- app.js
+++ app.js
@@ -2,16 +2,73 @@
 import { evaluateGuess } from './utils.js';
 
 // get elements
-const guess = document.getElementById('guess').value;
+const userGuess = document.getElementById('guess');
 const guessesLeftDiv = document.getElementById('guesses-left-div');
 const guessButton = document.getElementById('guess-button');
+const evaluationDiv = document.getElementById('evaluation-div');
+const alertDiv = document.getElementById('alert');
+const replayButton = document.getElementById('replay-button');
 
 // initialize state
 let guessesLeft = 4;
 let answer = Math.ceil(Math.random() * 20);
 
+console.log(answer);
 
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
-    guessesLeft--;
+
+    // validate that input isn't empty
+    if (!userGuess.value) {
+        alertDiv.textContent = "Enter a number my dude.";
+        console.log(guessesLeft);
+        return;
+    } else if (userGuess.value > 20 || userGuess.value < 0) {
+        alertDiv.textContent = "That number is not between 1 and 20!";
+        console.log(guessesLeft);
+        return;
+    } else {
+        guessesLeft--;
+        alertDiv.textContent = '';
+    
+        if (guessesLeft >= 0) {
+            const guessScore = evaluateGuess(userGuess.value, answer);
+            
+            if (guessScore === 'correct') {
+                evaluationDiv.textContent = 'YOU WIN!!';
+                guessesLeftDiv.textContent = '';
+                guessButton.classList.add('hidden');
+                userGuess.classList.add('hidden');
+                replayButton.classList.remove('hidden');
+            } else {
+                if (guessesLeft === 0) {
+                    evaluationDiv.textContent = 'YOU SUCK!!';
+                    guessesLeftDiv.textContent = `You have ${guessesLeft} guesses left.`;
+                    guessesLeftDiv.textContent = '';
+                    guessButton.classList.add('hidden');
+                    userGuess.classList.add('hidden');
+                    replayButton.classList.remove('hidden');
+                    return;
+                }
+                evaluationDiv.textContent = `Your number is too ${guessScore}`;
+                guessesLeftDiv.textContent = `You have ${guessesLeft} guesses left.`;
+            }
+        } else {
+            guessesLeftDiv.textContent = "You're all out of guesses dude!";
+            userGuess.value = '';
+        }
+
+    }
+
 });
+
+replayButton.addEventListener('click', () => {
+    guessButton.classList.remove('hidden');
+    userGuess.classList.remove('hidden');
+    replayButton.classList.add('hidden');
+    userGuess.value = '';
+    answer = Math.ceil(Math.random() * 20);
+    guessesLeft = 4;
+    evaluationDiv.textContent = '';
+    console.log(answer);
+})
--- utils.js
+++ utils.js
@@ -1,12 +1,15 @@
 export function evaluateGuess(guess, answer) {
     if (guess < answer) {
+        console.log('low');
         return 'low';
     }
 
     if (guess > answer) {
+        console.log('high');
         return 'high';
     }
 
+    console.log('correct');
     return 'correct';
 }
 
